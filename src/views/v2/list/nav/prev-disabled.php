e41c890dd7de976ded45243417df8db22b5955a9
Juanfra Aldasoro <juanfraa@gmail.com>
2019-07-12 16:04:19 -0300

    Add missing textdomain.

--- src/views/v2/list/nav/prev-disabled.php
+++ src/views/v2/list/nav/prev-disabled.php
@@ -21,7 +21,7 @@
 			$events_label = '<span class="tribe-events-c-nav__prev-label-plural"> ' . tribe_get_event_label_plural() . '</span>';
 			echo wp_kses(
 				/* translators: %s: Event (plural or singular). */
-				sprintf( __( 'Previous %1$s' ), $events_label ),
+				sprintf( __( 'Previous %1$s', 'the-events-calendar' ), $events_label ),
 				[ 'span' => [ 'class' => [] ] ]
 			);
 		?>
